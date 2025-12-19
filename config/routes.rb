f68409268ca3efde685b2e227a94b0e5833d497f
sugqwqrqP <sugqwqrqp@gmail.com>
2025-12-19 16:13:36 +0900

    add: ルートパスとリソースをoperator名前空間に追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :operator do
+    root "top#index"
+    resources :runs, only: [:index, :show, :new, :create]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
