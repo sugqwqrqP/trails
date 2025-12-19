94154c80059454782b19db526eba9495f6f4d103
sugqwqrqP <sugqwqrqp@gmail.com>
2025-12-20 01:40:00 +0900

    add: loginControllerにcreate,destroyアクションを追加

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -3,8 +3,19 @@ class LoginsController < ApplicationController
   end
 
   def create
+    user = User.find_by(login_id: params[:login_id])
+
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:alert] = "ログインIDまたはパスワードが違います"
+      render :new
+    end
   end
 
   def destroy
+    reset_session #session.delete(:user_id)よりもこちらが定石らしい
+    redirect_to root_path
   end
 end
