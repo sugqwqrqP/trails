d9c63d23cab3ac41231799473a7ba01698df4cf1
sugqwqrqP <sugqwqrqp@gmail.com>
2025-12-20 00:01:58 +0900

    add: SeatsControllerとビューを追加し、仮の座席選択機能を実装

---
+++ app/controllers/seats_controller.rb
@@ -0,0 +1,9 @@
+class SeatsController < ApplicationController
+  def index
+    @run = Run.find(params[:run_id])
+    @car_id = params[:car_id]
+
+    # 仮データ：座席ラベル
+    @seats = %w[A1 A2 A3 A4 A5 B1 B2 B3 B4 B5 C1 C2 C3 C4 C5]
+  end
+end
