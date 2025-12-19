a284d5b3a1fc581f23dea21cbdb2355ff2c3b0fb
sugqwqrqP <sugqwqrqp@gmail.com>
2025-12-19 23:07:53 +0900

    add: 仮のseedデータとして3本のrunを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+Run.create!(
+  name: "のぞみ1号",
+  run_number: 1,
+  run_on: Date.new(2025, 12, 25),
+  is_up: false,
+  departure_station_name: "東京",
+  arrival_station_name: "新大阪",
+  departure_time: "09:00",
+  arrival_time: "12:00"
+)
+
+Run.create!(
+  name: "のぞみ3号",
+  run_number: 3,
+  run_on: Date.new(2025, 12, 25),
+  is_up: false,
+  departure_station_name: "東京",
+  arrival_station_name: "新大阪",
+  departure_time: "10:00",
+  arrival_time: "13:00"
+)
+
+Run.create!(
+  name: "ひかり101号",
+  run_number: 101,
+  run_on: Date.new(2025, 12, 25),
+  is_up: false,
+  departure_station_name: "東京",
+  arrival_station_name: "新大阪",
+  departure_time: "10:10",
+  arrival_time: "13:30"
+)
