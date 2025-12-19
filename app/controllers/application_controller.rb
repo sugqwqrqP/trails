3e86998dfc8f2fcdeaa9a8ea4841f3b2ae615255
sugqwqrqP <sugqwqrqp@gmail.com>
2025-12-20 01:49:57 +0900

    add: ApplicationControllerにcurrent_userとlogged_in?メソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    return @current_user if defined?(@current_user)       # current_userが未定義なら2行目に入ってDBを叩く
+    @current_user = User.find_by(id: session[:user_id])   # 同じリクエスト内で何度もDBを叩かないための実装（メモ化）
+  end
+
+  def logged_in?
+    current_user.present?
+  end
 end
