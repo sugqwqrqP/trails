05008dd76fa8f520d11ffc9c14239059524fc138
sugqwqrqP <sugqwqrqp@gmail.com>
2025-12-19 17:17:14 +0900

    add: 利用者のルーティング設定を記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,50 @@
 Rails.application.routes.draw do
 
-  #運行管理者
+  #------------------------------
+
+  # ＜利用者＞
+
+  # トップ
+  root "top#index"
+
+  # 便
+  resources :runs, only: [] do
+    collection do
+      get :search
+    end
+
+    resources :cars, only: [:index] do
+      resources :seats, only: [:index]
+    end
+  end
+
+  # 予約
+  resources :reservations, only: [:create] do
+    get :confirm,  on: :collection
+    get :complete, on: :collection
+  end
+
+  # 利用者
+  resources :users, only: [:new, :create, :show, :edit, :update] do
+    resources :reservations, only: [:show, :destroy]
+  end
+
+  # ログイン（単数リソース）
+  resource :login, only: [:new, :create, :destroy]
+
+  #------------------------------
+
+  # ＜運行管理者＞
+
   namespace :operator do
     root "top#index"
     resources :runs, only: [:index, :show, :new, :create]
   end
 
-  #駅員
+  #------------------------------
+
+  # ＜駅員＞
+
   namespace :staff do
     root "top#index"
 
