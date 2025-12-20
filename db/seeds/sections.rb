b1432cd1a43c5d378bed8ed6071ee2dc9e160994
sugqwqrqP <sugqwqrqp@gmail.com>
2025-12-20 18:12:47 +0900

    add: sectionsテーブルに、実際に即したseedを設定

--- db/seeds/sections.rb
+++ db/seeds/sections.rb
@@ -1,29 +1,72 @@
+# db/seeds/sections.rb
 
-# 主キーが1,2,3のRunTypeオブジェクトを持ってくる
-# オブジェクトを入れても、belongs_toで関連付けされてれば、結局参照先の主キーに変換して保存されるのでOK
+# === RunType を取得 ===
+run_types = {
+  nozomi: RunType.find_by!(name: "のぞみ"),
+  hikari: RunType.find_by!(name: "ひかり"),
+  kodama: RunType.find_by!(name: "こだま")
+}
 
-nozomi = RunType.find(1)
-hikari = RunType.find(2)
-kodama = RunType.find(3)
+# === 停車駅定義===
+stations_by_type = {
+  nozomi: %w[東京 品川 新横浜 名古屋 京都 新大阪],
+  hikari: %w[東京 品川 新横浜 静岡 浜松 名古屋 京都 新大阪],
+  kodama: %w[東京 品川 新横浜 小田原 熱海 三島 新富士 静岡 掛川 浜松 豊橋 三河安城 名古屋 岐阜羽島 米原 京都 新大阪]
+}
 
-tokyo      = Station.find_by!(station_name: "東京")
-shinagawa  = Station.find_by!(station_name: "品川")
-shinyoko   = Station.find_by!(station_name: "新横浜")
+# === Station を名前→オブジェクトで引けるようにする ===
+station_map = Station.all.index_by(&:station_name)
 
-Section.create!(
-  run_type: nozomi,
-  from_station: tokyo,
-  to_station: shinagawa,
-  section_order: 1,
-  required_time: 6,
-  fee: 500
-)
+# === 区間ごとの所要時間・料金 ===
+section_data = {
+  nozomi: {
+    ["東京", "品川"]     => { time: 6,  fee: 500 },
+    ["品川", "新横浜"]   => { time: 11, fee: 1200 },
+    ["新横浜", "名古屋"] => { time: 82, fee: 7000 },
+    ["名古屋", "京都"]   => { time: 34, fee: 2500 },
+    ["京都", "新大阪"]   => { time: 14, fee: 1500 }
+  },
+  hikari: {
+    ["東京", "品川"]     => { time: 6,  fee: 400 },
+    ["品川", "新横浜"]   => { time: 11, fee: 600 },
+    ["新横浜", "静岡"]   => { time: 42, fee: 3000 },
+    ["静岡", "浜松"]     => { time: 20, fee: 1500 },
+    ["浜松", "名古屋"]   => { time: 30, fee: 2000 },
+    ["名古屋", "京都"]   => { time: 34, fee: 2000 },
+    ["京都", "新大阪"]   => { time: 14, fee: 1000 }
+  },
+  kodama: {
+    ["東京", "品川"]       => { time: 6,  fee: 300 },
+    ["品川", "新横浜"]     => { time: 11, fee: 400 },
+    ["新横浜", "小田原"]   => { time: 15, fee: 800 },
+    ["小田原", "熱海"]     => { time: 12, fee: 600 },
+    ["熱海", "三島"]       => { time: 8, fee: 400 },
+    ["三島", "新富士"]     => { time: 13, fee: 500 },
+    ["新富士", "静岡"]     => { time: 15, fee: 700 },
+    ["静岡", "掛川"]       => { time: 16, fee: 900 },
+    ["掛川", "浜松"]       => { time: 12, fee: 600 },
+    ["浜松", "豊橋"]       => { time: 16, fee: 1200 },
+    ["豊橋", "三河安城"]   => { time: 19, fee: 500 },
+    ["三河安城", "名古屋"] => { time: 16, fee: 400 },
+    ["名古屋", "岐阜羽島"] => { time: 16, fee: 500 },
+    ["岐阜羽島", "米原"]   => { time: 17, fee: 700 },
+    ["米原", "京都"]       => { time: 24, fee: 1200 },
+    ["京都", "新大阪"]     => { time: 17, fee: 800 }
+  }
+}
 
-Section.create!(
-  run_type: nozomi,
-  from_station: shinagawa,
-  to_station: shinyoko,
-  section_order: 2,
-  required_time: 12,
-  fee: 800
-)
+# === Section 作成 ===
+stations_by_type.each do |type, station_names|
+  station_names.each_cons(2).with_index(1) do |(from, to), order|
+    data = section_data.dig(type, [from, to])
+
+    Section.create!(
+      run_type: run_types[type],
+      from_station: station_map.fetch(from),
+      to_station: station_map.fetch(to),
+      section_order: order,
+      required_time: data[:time],
+      fee: data[:fee]
+    )
+  end
+end
