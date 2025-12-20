7c0a2cf5022d7191a8d9ca1ec8b9af423e1d1ea9
sugqwqrqP <sugqwqrqp@gmail.com>
2025-12-20 16:33:40 +0900

    add: run_types テーブルにseedを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,6 @@
 # db:seed / db:rebuild 実行時に以下の seed を順に読み込む
 
 load Rails.root.join("db/seeds/stations.rb")
+load Rails.root.join("db/seeds/run_types.rb")
 load Rails.root.join("db/seeds/users.rb")
 load Rails.root.join("db/seeds/runs.rb")
---
+++ db/seeds/run_types.rb
@@ -0,0 +1,5 @@
+RunType.create!([
+  { id: 1, name: "のぞみ" },
+  { id: 2, name: "ひかり" },
+  { id: 3, name: "こだま" }
+])
