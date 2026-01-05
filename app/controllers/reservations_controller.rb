64fbff99a468196c72fa26c7c2bbd9aa76a4454e
sugqwqrqP <sugqwqrqp@gmail.com>
2026-01-05 17:28:18 +0900

    fix: 予約確認・完了画面で複数座席を正しく受け渡し・表示できるよう修正

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,21 +2,39 @@ class ReservationsController < ApplicationController
   before_action :require_login #ログイン要求
   def confirm
     @run = Run.find(params[:run_id])
-    @car_id = params[:car_id]
-    @seat = params[:seat]
+    @car = Car.find(params[:car_id])
+
+    seat_ids =
+      case params[:seat_ids]
+      when String
+        params[:seat_ids].split(",")
+      else
+        params[:seat_ids]
+      end
+
+    @seats = Seat.where(id: seat_ids)
   end
 
   def create
     redirect_to complete_reservations_path(
       run_id: params[:run_id],
       car_id: params[:car_id],
-      seat: params[:seat]
+      seat_ids: params[:seat_ids]
     )
   end
 
   def complete
     @run = Run.find(params[:run_id])
-    @car_id = params[:car_id]
-    @seat = params[:seat]
+    @car = Car.find(params[:car_id])
+
+    seat_ids =
+      case params[:seat_ids]
+      when String
+        params[:seat_ids].split(",")
+      else
+        params[:seat_ids]
+      end
+
+    @seats = Seat.where(id: seat_ids)
   end
 end
