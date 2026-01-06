499245dbdd0e3d5d20017ce4a3f3279d03b32ff0
sugqwqrqP <sugqwqrqp@gmail.com>
2026-01-07 02:51:54 +0900

    add: userのマイページを仮実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_login
+  
   def new
   end
 
@@ -6,6 +8,16 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+
+    unless @user == current_user
+      redirect_to root_path, alert: "権限がありません"
+      return
+    end
+
+    @reservations = @user.reservations
+      .includes(:run, :departure_station, :arrival_station)
+      .order(created_at: :desc)
   end
 
   def edit
