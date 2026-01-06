85e84a9dd8a037a8fed8e961080a4f31f3b880d3
sugqwqrqP <sugqwqrqp@gmail.com>
2026-01-07 01:54:58 +0900

    add: 空席判定をシートマップに反映

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -4,6 +4,18 @@ class SeatsController < ApplicationController
     @run = Run.find(params[:run_id])
     @car = @run.cars.find(params[:car_id])
 
+    @departure_station = Station.find(params[:departure_station_id])
+    @arrival_station   = Station.find(params[:arrival_station_id])
+
     @seats = @car.seats.order(:row, :column)
+
+    @unavailable_seat_ids =
+      @seats.reject { |seat|
+        seat.available_for?(
+          run: @run,
+          departure_station: @departure_station,
+          arrival_station: @arrival_station
+        )
+      }.map(&:id)
   end
 end
