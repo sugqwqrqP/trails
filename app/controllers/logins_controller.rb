a1f1f169c62db120fbf19565042a0a6278258232
sugqwqrqP <sugqwqrqp@gmail.com>
2026-01-07 04:12:02 +0900

    fix: 駅員用アカウントでログインした際に駅員トップにリダイレクトされるように修正

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -7,7 +7,15 @@ class LoginsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to session.delete(:return_to) || root_path
+
+      if user.staff?
+        # 駅員は必ず staff 名前空間へ
+        session.delete(:return_to)
+        redirect_to staff_root_path
+      else
+        # 一般利用者のみ return_to を使う
+        redirect_to session.delete(:return_to) || root_path
+      end
     else
       flash.now[:alert] = "ログインIDまたはパスワードが違います"
       render :new
