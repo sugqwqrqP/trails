41197cdbe4bb4cad44a11889a76772fb4afb1525
sugqwqrqP <sugqwqrqp@gmail.com>
2026-01-09 14:21:07 +0900

    add: シードに運行管理者アカウントを追加

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -13,3 +13,11 @@ User.create!(
   password_confirmation: "StaffAdmin1234$",
   role: 1
 )
+
+User.create!(
+  login_id: "operator_admin",
+  user_fullname: "運行管理者",
+  password: "OperatorAdmin1234$",
+  password_confirmation: "OperatorAdmin1234$",
+  role: 2
+)
