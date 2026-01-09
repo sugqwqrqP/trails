698399867df030c2640ab6040b5519637ca23c47
sugqwqrqP <sugqwqrqp@gmail.com>
2026-01-10 00:13:35 +0900

    add: 会員情報を編集する動線を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
   before_action :require_login, except: [:new, :create]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
-  end
-
-  def create
   end
 
   def create
@@ -18,12 +16,30 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
+  def show
+    @user = User.find(params[:id])
+
+    if @user != current_user
+      redirect_to root_path, alert: "権限がありません"
+      return
+    end
+
+    @reservations = @user.reservations.includes(
+      :run,
+      :departure_station,
+      :arrival_station,
+      reservation_seats: :seat
+    )
   end
 
   def update
+    if @user.update(update_user_params)
+      redirect_to user_path(@user), notice: "会員情報を更新しました"
+    else
+      render :edit
+    end
   end
-
+  
   def destroy
   end
 
@@ -32,6 +48,21 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(
       :login_id,
+      :user_fullname,
+      :password,
+      :password_confirmation
+    )
+  end
+
+    def set_user
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to root_path, alert: "権限がありません"
+    end
+  end
+
+  def update_user_params
+    params.require(:user).permit(
       :user_fullname,
       :password,
       :password_confirmation
