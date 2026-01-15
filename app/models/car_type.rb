e1d9621b32e5fa89cf81d323280ae973cd27f6d4
sugqwqrqP <sugqwqrqp@gmail.com>
2026-01-15 18:04:07 +0900

    add: 予約一覧に席種を追加し、CarTypeに表示用ラベルを定義

--- app/models/car_type.rb
+++ app/models/car_type.rb
@@ -1,6 +1,15 @@
 class CarType < ApplicationRecord
   has_many :cars
 
+  def label
+    case name
+    when "reserved"  then "指定席"
+    when "green"     then "グリーン車"
+    when "fabulous"  then "ファビュラス"
+    else name
+    end
+  end
+
   def extra_fee
     case name
     when "reserved" then 0
